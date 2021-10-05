846597a7c4cfc6b2713f422fed86e99118e7dbc8
ashishsingh101 <ashish.singh.7424@gmail.com>
2021-10-05 13:17:52 +0530

    header

--- broker/broker/settings.py
+++ broker/broker/settings.py
@@ -128,6 +128,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
