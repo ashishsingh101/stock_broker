da03fd50570289f0395820b9cc4d27fcb321f922
ashishsingh101 <ashish.singh.7424@gmail.com>
2021-10-08 16:40:24 +0530

    updated nifty50 and niftyBank live data

--- broker/stock/views.py
+++ broker/stock/views.py
@@ -1,7 +1,20 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.conf import settings
+from nsetools import Nse
 
 # Create your views here.
 def dashboard(request):
+
+    if request.method == "POST" and request.POST['action']=='dashboard_data':
+        context = {}
+        nse = Nse()
+        index_nifty = nse.get_index_quote("nifty 50")
+        context['niftyPrice'] = index_nifty['lastPrice']
+        
+        index_niftyBank = nse.get_index_quote("nifty bank")
+        print(index_niftyBank)
+        context['niftyBankPrice'] = index_niftyBank['lastPrice']
+        return JsonResponse(context)
 
     return render(request, 'dashboard.html', {})
