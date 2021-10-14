0eb4091fcebdc366cdcf2f31520dfb4cf9ff7789
ashishsingh101 <ashish.singh.7424@gmail.com>
2021-10-15 01:05:14 +0530

    top gainers and losers 1

--- broker/broker/urls.py
+++ broker/broker/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- broker/stock/views.py
+++ broker/stock/views.py
@@ -11,10 +11,29 @@ def dashboard(request):
         nse = Nse()
         index_nifty = nse.get_index_quote("nifty 50")
         context['niftyPrice'] = index_nifty['lastPrice']
+        context['niftyChange'] = index_nifty['change']
+        context['niftyPChange'] = index_nifty['pChange']
         
         index_niftyBank = nse.get_index_quote("nifty bank")
-        print(index_niftyBank)
         context['niftyBankPrice'] = index_niftyBank['lastPrice']
+        context['niftyBankChange'] = index_niftyBank['change']
+        context['niftyBankPChange'] = index_niftyBank['pChange']
+
+        context['topGainers'] = nse.get_top_gainers()[:5]
+        context['topLosers'] = nse.get_top_losers()[:5]
+
+        logos = {}
+        for ele in context['topGainers']:
+            logos[ele['symbol']] = nse.get_quote(ele['symbol'])['isinCode']
+        for ele in context['topLosers']:
+            logos[ele['symbol']] = nse.get_quote(ele['symbol'])['isinCode']
+        context['logos'] = logos
+        print(logos)
+        """
+        d=nse.get_quote('ZOMATO')
+        for ele in d:
+            print(ele)
+        """ 
         return JsonResponse(context)
 
     return render(request, 'dashboard.html', {})
