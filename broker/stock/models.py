414f8fda2a27443d0afa46f9d7240cade9f8c042
ashishsingh101 <ashish.singh.7424@gmail.com>
2021-11-10 10:27:34 +0530

    dashboard

--- broker/stock/models.py
+++ broker/stock/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Investment(models.Model):
-    buy_price = models.IntegerField(max_length=1000)
+    buy_price = models.IntegerField(null=True, blank=True)
     date_time = models.DateTimeField(auto_now_add=True)
 
 class History(models.Model):
     investment = models.ForeignKey(Investment, on_delete=models.CASCADE)
-    sell_price = models.IntegerField(max_length=1000)
+    sell_price = models.IntegerField(null=True, blank=True)
     date_time = models.DateTimeField(auto_now_add=True)
 
 
