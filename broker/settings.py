cbaecf5daa3ff5b0210f38746c2c207ba84ee555
ashishsingh101 <ashish.singh.7424@gmail.com>
2021-12-17 00:29:53 +0530

    added hosts

--- broker/settings.py
+++ broker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y#ruxrt!-(%+=d-h&(8li&i$iy9y%bi25%fi!ye!$qsbcttxbv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://stockbrokerinvestment.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
