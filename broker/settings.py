46846f1618121c1aaca4881fbedd284ba752aa66
ashish singh <ashish.singh.7424@gmail.com>
2022-02-12 16:53:11 +0530

    some changes

--- broker/settings.py
+++ broker/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,4 +146,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT = 'dashboard'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
